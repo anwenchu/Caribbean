cb8c07b50e874c819c53be2ffee40af04b762f13
zhaowei <eblis.e.a.zhao@gmail.com>
2021-10-07 15:59:24 +0800

    1. spring config placeholder 2. gitignore 3. 优化数据拉取

--- src/main/java/com/anwenchu/caribbean/channel/huobi/repository/MarketRepository.java
+++ src/main/java/com/anwenchu/caribbean/channel/huobi/repository/MarketRepository.java
@@ -1,14 +1,17 @@
 package com.anwenchu.caribbean.channel.huobi.repository;
 
-import com.anwenchu.caribbean.channel.huobi.model.MarketDao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
+import com.anwenchu.caribbean.channel.huobi.model.MarketDao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface MarketRepository extends JpaRepository<MarketDao, Long> {
+
+
+public interface MarketRepository extends CrudRepository<MarketDao, Long> {
 
     @Query("select m from MarketDao m WHERE SUBSTRING(m.marketTime, 11) in (' 00:00:00') ORDER BY m.marketTime DESC ")
     List<MarketDao> listAllOrderByMarketTime();
@@ -21,5 +24,7 @@ public interface MarketRepository extends JpaRepository<MarketDao, Long> {
 
     @Query("select COUNT(m.id) from MarketDao m WHERE SUBSTRING(m.marketTime, 1, 10) =:day AND  m.symbol =:symbol")
     int getCountByDay(@Param("day") String day, @Param("symbol")String symbol);
+
+    Page<MarketDao> findBySymbolOrderByMarketTimeDesc(String symbol, Pageable pageable);
 
 }
--- src/main/java/com/anwenchu/caribbean/job/huobi/HuoBiMarketJob.java
+++ src/main/java/com/anwenchu/caribbean/job/huobi/HuoBiMarketJob.java
@@ -1,27 +1,42 @@
 package com.anwenchu.caribbean.job.huobi;
 
 
-import com.anwenchu.caribbean.channel.huobi.config.HuoBiArbitrageConfig;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+
 import com.anwenchu.caribbean.channel.huobi.enums.CandlestickIntervalEnum;
+import com.anwenchu.caribbean.channel.huobi.model.MarketDao;
+import com.anwenchu.caribbean.channel.huobi.model.SymbolDao;
 import com.anwenchu.caribbean.channel.huobi.order.request.ReqCandlestickRequest;
 import com.anwenchu.caribbean.channel.huobi.order.response.Candlestick;
 import com.anwenchu.caribbean.channel.huobi.order.service.Client;
-import com.anwenchu.caribbean.channel.huobi.model.MarketDao;
 import com.anwenchu.caribbean.channel.huobi.repository.MarketRepository;
 import com.anwenchu.caribbean.channel.huobi.repository.SymbolRepository;
 import com.anwenchu.caribbean.utils.DateUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
-
-import java.math.RoundingMode;
-import java.util.Date;
-import java.util.List;
 
 @Component
 @Slf4j
 public class HuoBiMarketJob {
+
+    @Value("${com.anwenchu.fetch.num:300}")
+    private int fetchNum;
+
+    @Value("${com.anwenchu.fetch.sleep:1000}")
+    private int sleepTime;
+
+    @Value("${com.anwenchu.fetch.symbol:btcusdt}")
+    private String fetchSymbol;
 
     @Autowired
     private MarketRepository marketRepository;
@@ -30,53 +45,79 @@ public class HuoBiMarketJob {
     private Client client;
 
     @Autowired
-    private HuoBiArbitrageConfig config;
-
-    @Autowired
     private SymbolRepository symbolRepository;
 
-    private volatile boolean running;
+    @PostConstruct
+    public void start() {
+        List<SymbolDao> symbolDaos = symbolRepository.findBySymbol(fetchSymbol);
+
+        //不知道为啥，这里不save一次，下面的程序执行就无法保存
+        MarketDao marketDao = MarketDao.builder()
+                .marketDay("").marketTime(new Date()).amount(BigDecimal.ZERO)
+                .close(BigDecimal.ZERO).open(BigDecimal.ZERO).high(BigDecimal.ZERO).low(BigDecimal.ZERO)
+                .count(BigDecimal.ZERO).arbitrage(BigDecimal.ZERO).vol(BigDecimal.ZERO).symbol("test")
+                .build();
+        marketRepository.save(marketDao);
 
 
-    @Scheduled(fixedDelay = 120000)
-    public void huoBiZeroMarket() {
-        symbolRepository.findAll().subList(config.getStarId(), config.getEndId()).forEach(symbol -> {
-            Date now = DateUtils.getTime(1632930600000L);
-            Date before = DateUtils.getBeforeYear(now, -1);
-            while (now.after(before)) {
-                Date toDate = DateUtils.getAfterMinute(now, 2000);
-                Long to = toDate.getTime()/1000;
-                Long from = now.getTime()/1000;
-                String day = DateUtils.formatDateYYYYMMDD(now);
-                if (marketRepository.getCountByDay(day, symbol.getSymbol()) < 21 &&
-                        symbol.getTradeOpenAt().before(now) && !day.equalsIgnoreCase("2020-09-27")) {
-                    log.warn("获取交易对:{} 在时间:{} ~ {}的K线", symbol.getSymbol(), now, toDate);
-                    try {
-                        client.reqCandlestick(ReqCandlestickRequest.builder()
-                                .symbol(symbol.getSymbol())
-                                .from(from)
-                                .to(to)
-                                .interval(CandlestickIntervalEnum.MIN1)
-                                .build(), candlestickReq -> {
-                            saveMarketDao(candlestickReq.getCandlestickList(), symbol.getSymbol());
-                        });
-                    } catch (Exception e) {
-                        log.error("error: ", e);
-                    }
-                }
-                now = DateUtils.getBeforeDay2(now);
-            }
-
-        });
+        huoBiZeroMarket(symbolDaos.get(0));
     }
 
+
+    @SuppressWarnings("BusyWait")
+    private void huoBiZeroMarket(SymbolDao symbol) {
+
+        long startTime = symbol.getTradeOpenAt().getTime();
+
+        PageRequest pageRequest = PageRequest.of(0, 1);
+
+        Page<MarketDao> marketDaos =
+                marketRepository.findBySymbolOrderByMarketTimeDesc(symbol.getSymbol(), pageRequest);
+        if (Objects.nonNull(marketDaos) && marketDaos.getContent().size() > 0) {
+            MarketDao marketDao = marketDaos.getContent().get(0);
+            long fetchTime = marketDao.getMarketTime().getTime();
+            startTime = Math.max(startTime, fetchTime);
+        }
+
+        long start = startTime / 1000;
+        // 1min 线 300 根
+        long end = start + (fetchNum * 60);
+
+        while (System.currentTimeMillis() > (end * 1000)) {
+            log.warn("获取交易对:{} 在时间:{} ~ {}的K线", symbol.getSymbol(), start, end);
+            try {
+                client.reqCandlestick(ReqCandlestickRequest.builder()
+                        .symbol(symbol.getSymbol())
+                        .from(start)
+                        .to(end)
+                        .interval(CandlestickIntervalEnum.MIN1)
+                        .build(),
+                        candlestickReq -> saveMarketDao(candlestickReq.getCandlestickList(), symbol.getSymbol()));
+
+                Thread.sleep(sleepTime);
+                start = end;
+                end = start + (fetchNum * 60);
+            } catch (Exception ioe) {
+                log.error("error: ", ioe);
+            }
+        }
+    }
+
+
     private void saveMarketDao(List<Candlestick> candlestickList, String symbol) {
-        candlestickList.forEach(candlestick -> {
+        List<Candlestick> data;
+        if (candlestickList.size() > fetchNum) {
+            // 扔掉第一个元素，
+            data = candlestickList.subList(1, candlestickList.size());
+        } else {
+            data = candlestickList;
+        }
+        data.forEach(candlestick -> {
             MarketDao marketDao = buildMarketDao(candlestick, symbol);
             try {
                 marketRepository.save(marketDao);
-            } catch (Exception e) {
-                log.error("数据已存在 {}", candlestick);
+            } catch (Throwable e) {
+                log.error("数据已存在 {}", candlestick, e);
             }
         });
     }
